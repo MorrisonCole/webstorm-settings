d3409f44ae06b533638c005d094c8f92ce674618
Morrison Cole <admin@morrisoncole.co.uk>
2019-11-18 07:45:05 +0900

    WS-193.5233.13 <admin@DESKTOP-GSNIBJ5 Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, React.xml, Default.xml, project.default.xml, laf.xml, Default.xml, React Material UI Component.js, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, vcs.xml, colors.scheme.xml, notifications.xml, React Component.js, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/React Component.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class $NAME extends Component {
+  render() {
+    return (
+      <div>
+        $END
+      </div>
+    )
+  }
+}
+
+export default $NAME
---
+++ fileTemplates/React Material UI Component.js
@@ -0,0 +1,16 @@
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import React from "react";
+
+const useStyles = makeStyles(theme => ({
+  
+}));
+
+export default function $Title$() {
+  const classes = useStyles();
+
+  return (
+    <$COMPONENT>
+      $END
+    </$COMPONENT>
+  );
+}
